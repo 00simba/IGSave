a781ef2f552cf71753989467ed866d91587247ce
00simba <sameer15557@gmail.com>
2023-02-11 16:59:39 -0500

    Updated request headers

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ import urllib.request, requests
 import json
 from flask_cors import CORS, cross_origin
 import base64
+import time
 
 application = Flask(__name__)
 
@@ -29,22 +30,30 @@ def index():
         else:
             download_url = download_url[0:40]
 
-        s = requests.Session()
-
         headers = {
-                "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",			
-                "Accept-Encoding" : "gzip, deflate, br",	
-                "Accept-Language" : "en-CA,en-US;q=0.7,en;q=0.3",		
-                "Connection" : "keep-alive",
-                "Cookie" : 'fbm_124024574287414=base_domain=.instagram.com; mid=Ykx7ZgALAAEuA5ym-037fLHn17yE; ig_did=1878DCE3-1DD2-4E8A-AEBF-F030FB4E39C5; ig_nrcb=1; datr=QDcAY3K6EgsSgRBNdwVvBkB-; dpr=2; csrftoken=rKnymk7cq5nAt6rePZgHvVL9loa57V3O; ds_user_id=2237186360; shbid="2028\0542237186360\0541705536455:01f7487ae8e73d18f8ddb60a0d0429aafc23a5e1b6c1a0aca398efe66072142cafabc4c8"; shbts="1674000455\0542237186360\0541705536455:01f748d6e03da490e7a14c26af3a9c973073d8834f556048578bca0be3b1f43d61935108"; fbsr_124024574287414=2VyviBh4z0udNoK0rCU-5yJSfKdzItiXiAhSvqHYQfY.eyJ1c2VyX2lkIjoiMTAwMDE0MjI4OTgzMDMzIiwiY29kZSI6IkFRQTNtVllkY2FEaFRaQkhUaTU3eG9xRzg5OE80dmZVbEZBSDRIeWo0M3d0bGpsOGdhNXZJRUNYOUp3R0JjV09VRUFKOGNicDVvdHBNVXhfUkFNZFJrQVpTQ1Q0VFRlUktoTTFXUFdYRktnQ29XSEpOMVdfOFNMOXR0OXVhVkxEVHdHSktMeWVlalVTU1VuY3BvZy1nN3MwbnZvTGJmR3BVbmxWYTdSeUIwbDNNQl9pWVVWNnloMXdYZ0tkX3NUWlY3V2JxYzJxOE1WMmlqTWszd1dkbkRHOHBjWmJQUnJMVnJUVVZMU0dGb25jQXlXQ2F2NVViYmYyclRBZ1BoQkxwWWE3WkdNNDI1U0cxMzZIaTRIS3JtNTFxa2JwaGY4YmlGejZET2laVU5wQjJBSmU5Tl9WYVREZEFRb2ZOSFFtbW9Xakh5OUhCMm1MTnFwU0NlVmVPc0JWIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQURmSWVCaXRPSTMxZlpCZDFNWkNYR1pBUGRlMnNJbUlCRTBFZ3I2TVpDVGxpbmJJUG9zZUNXNzdrcTZDM1YzdjJUT29tWkMwMmhZV2dCTFluWkE1N3RNTXVHblRoWkFmWGZWTzI2bVpBTVpCMVhJQWk2WEpHUE1HOWVNQzY5eTJnR05XNFBSczJoWUd3VE45RW1ONm9DU3RwRExaQU5oZDA4R0JRcHhJTUpoN0RMa3RLTGFqWHRueGdaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjc0MDExOTExfQ; sessionid=2237186360:io1cFi0imrNrM5:21:AYfrgXg3dnVd1NnuIF0XxWZTgP5B3yHLITLYAs-Tsw; fbsr_124024574287414=2VyviBh4z0udNoK0rCU-5yJSfKdzItiXiAhSvqHYQfY.eyJ1c2VyX2lkIjoiMTAwMDE0MjI4OTgzMDMzIiwiY29kZSI6IkFRQTNtVllkY2FEaFRaQkhUaTU3eG9xRzg5OE80dmZVbEZBSDRIeWo0M3d0bGpsOGdhNXZJRUNYOUp3R0JjV09VRUFKOGNicDVvdHBNVXhfUkFNZFJrQVpTQ1Q0VFRlUktoTTFXUFdYRktnQ29XSEpOMVdfOFNMOXR0OXVhVkxEVHdHSktMeWVlalVTU1VuY3BvZy1nN3MwbnZvTGJmR3BVbmxWYTdSeUIwbDNNQl9pWVVWNnloMXdYZ0tkX3NUWlY3V2JxYzJxOE1WMmlqTWszd1dkbkRHOHBjWmJQUnJMVnJUVVZMU0dGb25jQXlXQ2F2NVViYmYyclRBZ1BoQkxwWWE3WkdNNDI1U0cxMzZIaTRIS3JtNTFxa2JwaGY4YmlGejZET2laVU5wQjJBSmU5Tl9WYVREZEFRb2ZOSFFtbW9Xakh5OUhCMm1MTnFwU0NlVmVPc0JWIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQURmSWVCaXRPSTMxZlpCZDFNWkNYR1pBUGRlMnNJbUlCRTBFZ3I2TVpDVGxpbmJJUG9zZUNXNzdrcTZDM1YzdjJUT29tWkMwMmhZV2dCTFluWkE1N3RNTXVHblRoWkFmWGZWTzI2bVpBTVpCMVhJQWk2WEpHUE1HOWVNQzY5eTJnR05XNFBSczJoWUd3VE45RW1ONm9DU3RwRExaQU5oZDA4R0JRcHhJTUpoN0RMa3RLTGFqWHRueGdaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjc0MDExOTExfQ; rur="NCG\0542237186360\0541705547949:01f76a58f58bbf5af703c726f884f2c08d5fcde6e34db00299afba65e78cbe8b1b1bfa20"',
-                "Host" : "www.instagram.com",   
-                "Sec-Fetch-Dest" : "document",
-                "Sec-Fetch-Mode" : "navigate",
-                "Sec-Fetch-Site" : "none",		
-                "Sec-Fetch-User" : "?1",		
-                "TE" : "trailers",     
-                "Upgrade-Insecure-Requests" : "1",
-                "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"		
+            'authority': 'www.instagram.com',
+            'method': 'GET',
+            'path': '/api/v1/web/login_page/',
+            'scheme': 'https',
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9',
+            'cookie': 'fbm_124024574287414=base_domain=.instagram.com; mid=Ykx7ZgALAAEuA5ym-037fLHn17yE; ig_did=1878DCE3-1DD2-4E8A-AEBF-F030FB4E39C5; ig_nrcb=1; datr=QDcAY3K6EgsSgRBNdwVvBkB-; dpr=2; shbid="2028\0542237186360\0541707683625:01f7ae4f7f24983f285a96393de06cc32792957a8d41b9bcfdc8eca2a3a3018a03d243f0"; shbts="1676147625\0542237186360\0541707683625:01f7f66d20d530ba09c48b4d372a077bb63d2be9b5371f2bdeafd2647223240ec17750b9"; rur="EAG\0542237186360\0541707683829:01f7fe6ca4f31bba2a664ed3a8ca58ed068e4aee89c33b9afb81001e9302883f61388290"; csrftoken=v1MeTUrYWEIQEpbMG64RGxLfEhKblygq; fbsr_124024574287414=5Rg6oWgoAvWiGwNCST2DSueNiKiBO0q5OcOuVgkMWok.eyJ1c2VyX2lkIjoiMTAwMDE0MjI4OTgzMDMzIiwiY29kZSI6IkFRQ2lPbWhMWElQcEtpc2NKSWJoU1FyQzRpdGM3WC1mcXR6Q0Z1TlZ1dkhnNjMzYmcweExCWFN1NVV4OURtV05PUkhOOGZDRm1fMGEtTUhkY2U3elRmRWU5Q1h3NEwxS0RTOXktZ25UQzdaeUZxQk9uMDB4QVVId1BLbEFZNzFic09INjVmano0bG5UcXBydTNJeUxFMmxqWXpTWEhCb05XdjVjYWdsMlF0bklaM05YUDRmME1KQ25ndUc3Um5XNnFyelREVHJBMkdoWXo5Y0IxX3RDNXEydi1RYTlmMUljeFcxbkktY3pEcHh2VkFwSUhaelVrZ0dUZkNFSmFuQ0syYlJDVzB3cFlLZ2k0VG5tQTlmOW0xakhjWjU3anItNUNldUgyRXNhY2pDeDJoRVZaSFpwRUFuOXJOV1ktY1NodUtEYkh3bGxHUm1La292LUNZQlFPWU5FLUVuQUYzSFNNSENSeFd1bTNFTURMdyIsIm9hdXRoX3Rva2VuIjoiRUFBQnd6TGl4bmpZQkFEWGZkOFhMaEZMNlluWVhXS3RaQTI1RGlLSDhPNHRoS21UTXVwTXRuWTlORHpiRkZVWkF5QWR1WEFZbGg3RFpCekJlY2lrMHFVM1pCTVpBeU1BQ2RmTDFNQ1Jod0dBNEFVdkdrbWs4VVhJSXM1WkMwclFEZ2xKdExuT1pDZ0RsWkFoOFlxQXVJMkdjY1NYd1hxZGpBbXZhREUweGkxVzZPWWtzVWU4WkMxbkV6cDBLSEE2SXJNNllaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjc2MTQ4MTQ4fQ',
+            'referer': 'https://www.instagram.com/accounts/login/',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36',
+            'viewport-width': '1440',
+            'x-asbd-id': '198387',
+            'x-csrftoken': 'v1MeTUrYWEIQEpbMG64RGxLfEhKblygq',
+            'x-ig-app-id': '1217981644879628',
+            'x-ig-www-claim': 'hmac.AR2cAdvVLtr0e7cXsLAoALArvOFYjFH8FyQWT53etA8ZkoYK',
+            'x-requested-with': 'XMLHttpRequest'       	
         }
 
         data = {
@@ -52,17 +61,18 @@ def index():
                 'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:1589682409:'+ os.getenv('PASSWORD'),
                 'queryParams': '{}',
                 'optIntoOneTap': 'false'
-        }   
+        } 
 
+        s = requests.Session()
+            
         r = s.get(download_url + '?__a=1&__d=dis', headers=headers)
 
         if(r.status_code != 200):
             res = s.post('https://www.instagram.com/accounts/login/ajax/', headers=headers, data=data)
             r = s.get(download_url + '?__a=1&__d=dis', headers=headers)
-        
-        all_links.clear()
 
         media = r.json()
+
         mediaArray = []
 
         try:
