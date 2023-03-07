213c5bcda185bf3237ed384533a37a06098ca63c
00simba <sameer15557@gmail.com>
2023-03-07 01:04:46 -0500

    Testing login API

--- application.py
+++ application.py
@@ -14,6 +14,30 @@ cors = CORS(application)
 application.config['CORS_HEADERS'] = 'Content-Type'
 load_dotenv()
 
+data = {
+        'enc_password': f'#PWD_INSTAGRAM_BROWSER:10:{time}:'+ os.getenv('PASSWORD'),
+        'username': f'' + os.getenv('USER'),
+        'queryParams': '{}',
+        'optIntoOneTap': 'false',
+        'trustedDeviceRecords': '{}',
+} 
+
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "x-requested-with": "XMLHttpRequest",
+    "referer": "https://www.instagram.com/accounts/login/",
+    "x-csrftoken":'IbEsGeeenFTtKHolyV0q9kT3feMosvvW'
+}
+
+userAgent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36'
+
+s = requests.Session()
+s.headers = {'userAgent' : userAgent}
+s.headers.update({'Referer': 'https://www.instagram.com/accounts/login/'})
+r = s.post('https://www.instagram.com/accounts/login/ajax/', data=data, headers=headers)
+print(r.content)
+
+
 @application.route('/', methods = ['POST', 'GET'])
 @cross_origin()
 def index():
@@ -31,60 +55,36 @@ def index():
         else:
             download_url = download_url[0:40]
 
-        data = {
-                'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:'+ os.getenv('PASSWORD'),
-                'username': f'' + os.getenv('USER'),
-                'queryParams': '{}',
-                'optIntoOneTap': 'false',
-                'trustedDeviceRecords': '{}',
-        } 
+        
+        r = s.get(download_url + '?__a=1&__d=dis', headers=headers)
 
-        headers = {
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-            "x-requested-with": "XMLHttpRequest",
-            "referer": "https://www.instagram.com/accounts/login/",
-            "x-csrftoken":'IbEsGeeenFTtKHolyV0q9kT3feMosvvW'
-        }
+        media = r.json()
+        mediaArray = []
 
-        userAgent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36'
+        try:
+            #Checks to see if post is a reel
+            responseImg = requests.get(media['items'][0]['image_versions2']['candidates'][0]['url'])
+            responseVid = requests.get(media['items'][0]['video_versions'][0]['url'])
+            all_links.append({'url': media['items'][0]['video_versions'][0]['url'], 'base64': "data:" + responseImg.headers['Content-Type'] + ";" + "base64," + base64.b64encode(responseImg.content).decode("utf-8"), 'base64Vid': "data:" + responseVid.headers['Content-Type'] + ";" + "base64," + base64.b64encode(responseVid.content).decode("utf-8")})
+        except:
+            mediaArray = media['items'][0]
 
-        with requests.Session() as s:
-
-            s.headers = {'userAgent' : userAgent}
-            s.headers.update({'Referer': 'https://www.instagram.com/accounts/login/'})
-
-            r = s.get('https://www.instagram.com/accounts/login/')
-            r = s.post('https://www.instagram.com/accounts/login/ajax/', data=data, headers=headers)
-            r = s.get(download_url + '?__a=1&__d=dis')     
-
-            media = r.json()
-
-            mediaArray = []
-
+        if len(mediaArray):
             try:
-                #Checks to see if post is a reel
-                responseImg = requests.get(media['items'][0]['image_versions2']['candidates'][0]['url'])
-                responseVid = requests.get(media['items'][0]['video_versions'][0]['url'])
-                all_links.append({'url': media['items'][0]['video_versions'][0]['url'], 'base64': "data:" + responseImg.headers['Content-Type'] + ";" + "base64," + base64.b64encode(responseImg.content).decode("utf-8"), 'base64Vid': "data:" + responseVid.headers['Content-Type'] + ";" + "base64," + base64.b64encode(responseVid.content).decode("utf-8")})
+                for items in mediaArray['carousel_media']:  
+                    response = requests.get(items['image_versions2']['candidates'][0]['url'])
+                    try:
+                        #Checks to see if carousel media is a video
+                        all_links.append({'url': items['video_versions'][0]['url'], 'base64': "data:" + response.headers['Content-Type'] + ";" + "base64," + base64.b64encode(response.content).decode("utf-8")})
+                    except:
+                        #If not video, carousel media is an image
+                        all_links.append({'url': items['image_versions2']['candidates'][0]['url'], 'base64' : "data:" + response.headers['Content-Type'] + ";" + "base64," + base64.b64encode(response.content).decode("utf-8")})
             except:
-                mediaArray = media['items'][0]
-
-            if len(mediaArray):
-                try:
-                    for items in mediaArray['carousel_media']:  
-                        response = requests.get(items['image_versions2']['candidates'][0]['url'])
-                        try:
-                            #Checks to see if carousel media is a video
-                            all_links.append({'url': items['video_versions'][0]['url'], 'base64': "data:" + response.headers['Content-Type'] + ";" + "base64," + base64.b64encode(response.content).decode("utf-8")})
-                        except:
-                            #If not video, carousel media is an image
-                            all_links.append({'url': items['image_versions2']['candidates'][0]['url'], 'base64' : "data:" + response.headers['Content-Type'] + ";" + "base64," + base64.b64encode(response.content).decode("utf-8")})
-                except:
-                    #If the post is only a single image
-                    response = requests.get(mediaArray['image_versions2']['candidates'][0]['url'])
-                    all_links.append({'url': mediaArray['image_versions2']['candidates'][0]['url'], 'base64': "data:" + response.headers['Content-Type'] + ";" + "base64," + base64.b64encode(response.content).decode("utf-8")})
-            
-            return {'links' : all_links}
+                #If the post is only a single image
+                response = requests.get(mediaArray['image_versions2']['candidates'][0]['url'])
+                all_links.append({'url': mediaArray['image_versions2']['candidates'][0]['url'], 'base64': "data:" + response.headers['Content-Type'] + ";" + "base64," + base64.b64encode(response.content).decode("utf-8")})
+        
+        return {'links' : all_links}
         
     else:
         return {'links' : all_links}
