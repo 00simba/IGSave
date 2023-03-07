0c917c8c9c7903e89c72028a212163e21ccff995
00simba <sameer15557@gmail.com>
2023-03-06 21:10:19 -0500

    Added ToS and Privacy Policy

--- client/pages/terms-and-conditions.tsx
+++ client/pages/terms-and-conditions.tsx
@@ -29,17 +29,17 @@ export default function Home() {
             <br/>
             <p>Welcome to IGSave.io!</p>
             <br/>
-            <p>These terms and conditions outline the rules and regulations for the use of IGSave's Website, located at www.igsave.io.</p>
+            <p>These terms and conditions outline the rules and regulations for the use of IGSave`s Website, located at www.igsave.io.</p>
 
             <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use IGSave.io if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+            <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: `Client`, `You` and `Your` refers to you, the person log on this website and compliant to the Company’s terms and conditions. `The Company`, `Ourselves`, `We`, `Our` and `Us`, refers to our Company. `Party`, `Parties`, or `Us`, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
             <br/>
 
             <h3><strong>Cookies</strong></h3>
             <br/>
 
-            <p>We employ the use of cookies. By accessing IGSave.io, you agreed to use cookies in agreement with the IGSave's Privacy Policy. </p>
+            <p>We employ the use of cookies. By accessing IGSave.io, you agreed to use cookies in agreement with the IGSave`s Privacy Policy. </p>
 
             <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
             <br/>
@@ -57,7 +57,6 @@ export default function Home() {
                   <li>Redistribute content from IGSave.io</li>
               </ul>
 
-            <p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a href="https://www.termsandconditionsgenerator.com/">Free Terms and Conditions Generator</a>.</p>
             <br/>
             <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. IGSave does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of IGSave,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, IGSave shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
             <br/>
@@ -118,7 +117,7 @@ export default function Home() {
                 <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.</li>
             </ul>
             <br/>
-            <p>No use of IGSave's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+            <p>No use of IGSave`s logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
             <br/>
             <h3><strong>iFrames</strong></h3>
             <br/>
