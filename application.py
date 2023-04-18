fec5ed30acc7e97a9fb64423f03f67ab769b97b0
00simba <sameer15557@gmail.com>
2023-04-18 03:40:46 -0400

    Implemented auto cookie update

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ from flask_cors import CORS, cross_origin
 import base64
 import time
 import backoff
+import re
 
 application = Flask(__name__)
 
@@ -73,9 +74,66 @@ getHeaders = {
     'viewport-width': '425'
 }
 
+updateHeaders = {
+    'authority': 'www.instagram.com',
+    'method': 'GET',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0', 
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '425'
+}
+
 s = requests.Session()
-r = s.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/', data=data, headers=headers)
-print(r.content)
+
+def login():
+    
+    r = s.get('https://www.instagram.com/api/v1/web/accounts/login/ajax/', headers=updateHeaders)
+
+    newCookies = r.cookies.get_dict()
+
+    #Update headers
+
+    currentCookie = headers['cookie']
+    #Replace csrf
+    currentCookie = re.sub("csrftoken=(.*?)\;", 'csrftoken=' + newCookies['csrftoken'] + ';', currentCookie)
+    #Replace ig_did
+    currentCookie = re.sub("ig_did=(.*?)\;", 'ig_did=' + newCookies['ig_did'] + ';', currentCookie)
+    #Replace mid
+    currentCookie = re.sub("mid=(.*?)\;", 'mid=' + newCookies['mid'] + ';', currentCookie)
+    #Replace headers with new strings
+    headers['x-csrftoken'] = newCookies['csrftoken']
+    headers['cookie'] = currentCookie
+
+    #Update getHeaders
+
+    getHeadersCookie = getHeaders['cookie']
+    #Replace csrf
+    getHeadersCookie = re.sub("csrftoken=(.*?)\;", 'csrftoken=' + newCookies['csrftoken'] + ';', getHeadersCookie)
+    #Replace ig_did
+    getHeadersCookie = re.sub("ig_did=(.*?)\;", 'ig_did=' + newCookies['ig_did'] + ';', getHeadersCookie)
+    #Replace mid
+    getHeadersCookie = re.sub("mid=(.*?)\;", 'mid=' + newCookies['mid'] + ';', getHeadersCookie)
+    #Replace getHeadesr with new string
+    getHeaders['cookie'] = getHeadersCookie
+
+    #Finally login
+    r = s.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/', data=data, headers=headers)
+
+    print(r.content)
+
+login()
 
 @backoff.on_exception(backoff.expo, requests.exceptions.ConnectionError)
 def getJSON(url):
@@ -102,6 +160,13 @@ def index():
         r = getJSON(download_url + '?__a=1&__d=dis')
 
         media = r.json()
+
+        #Check if media array is valid, otherwise login() and getJSON()
+        if 'items' not in media:
+            login()
+            r = getJSON(download_url + '?__a=1&__d=dis')
+            media = r.json()
+
         mediaArray = []
 
         try:
